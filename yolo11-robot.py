ad678e0cf429b66976a9cd11b7b972cfa5281154
Yuan Liu <n11436361@qut.edu.au>
2025-05-21 09:25:14 +1000

    Use model yolo11.pt

--- yolo11-robot.py
+++ yolo11-robot.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO('yolo11n-pose.pt') 
+model = YOLO('yolo11n.pt') 
 
 model.train(
     data='robot_dataset/data.yaml',
     epochs=100,
     imgsz=640,
     batch=16,
-    name='robot_yolo11_pose_model_n',
-    project='yolo11_pose_training'
+    name='robot_yolo11_model_n',
+    project='yolo11_training'
 )
